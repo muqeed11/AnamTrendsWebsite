24788050b18aad0bd418e3128a11da108687bc7a
muqeed11 <33509947+muqeed11@users.noreply.github.com>
2020-09-21 12:41:22 +0530

    changes

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -14,6 +14,27 @@ export class ProductDetailsComponent implements OnInit {
   productId;
   customOptions: OwlOptions;
   whatsappApi= "https://wa.me/919000748372?text=";
+
+  pr =
+    {
+      "brand" : "PEARL DAISY",
+      "desc" : "Top  HEAVY FOX GEORGETT FRONT AND BACK EMBROIDERY WORK AND HAND PEARL WORK Bottom -  HEAVY ANTOON WITH HEAVY EMBROIDERY PATCHWORK  INNER - HEAVY SANTOON DUPATTA: -HEAVY NAZNEEN WITH EMBROIDERY WORK ",
+      "files" :
+        [{
+          "id":"1",
+          "url":"https://firebasestorage.googleapis.com/v0/b/anamtrends-d56c9.appspot.com/o/0.jpg?alt=media&token=91e763fe-8a9c-4c7f-8a1d-5fe3dfdfc4a2",
+        },
+
+
+        ],
+       "price" : "1399",
+      "seq" : 0,
+      "type" : "Stiched"
+    };
+
+
+
+
   productDetails : Observable<any[]>;
   constructor(private af:AngularFireDatabase,
               private route: ActivatedRoute,
@@ -31,25 +52,17 @@ export class ProductDetailsComponent implements OnInit {
 
   this.customOptions  = {
       loop: true,
-      mouseDrag: false,
-      touchDrag: false,
-      pullDrag: false,
-      dots: false,
+      mouseDrag: true,
+      touchDrag: true,
+      pullDrag: true,
+      dots: true,
       navSpeed: 700,
       navText: ['', ''],
       responsive: {
-        0: {
+        1: {
           items: 1
         },
-        400: {
-          items: 2
-        },
-        740: {
-          items: 3
-        },
-        940: {
-          items: 4
-        }
+
       },
       nav: true
     }
